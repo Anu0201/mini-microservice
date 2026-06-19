f63e22e1dc0cb69cf1c0fda2804ca656cda59985
Anu0201 <aaannnuuuxxx@gmail.com>
2026-06-19 11:34:28 +0800

    Feat: built in Docker

--- auth-service/src/main/java/com/anudari/auth_service/feign/UserClient.java
+++ auth-service/src/main/java/com/anudari/auth_service/feign/UserClient.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "user-service", url = "${user-service.url}")
 public interface UserClient {
     //login hiihed hereglegchiin password iig shalgahiin tuld user-service-s medeelel avah heregtei.
-    //credentialHash-iig avch password shalgadag dotood internal holboosoos eureka-aar user-service-g haij holbono
+    //credentialHash-iig avch password shalgadag dotood internal holboosoos user-service-g shuud holbono
     @GetMapping("/api/users/internal/by-username/{username}")
     UserInternalDto findByUsernameInternal(
             @PathVariable String username,
