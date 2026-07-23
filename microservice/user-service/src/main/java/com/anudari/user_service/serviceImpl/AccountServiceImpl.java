259a78b4b4a9c2d5391cbe80b7cce0bc87fb60f8
Anu0201 <aaannnuuuxxx@gmail.com>
2026-07-23 14:38:53 +0800

    Feat: Log utility servuce implemetation

--- microservice/user-service/src/main/java/com/anudari/user_service/serviceImpl/AccountServiceImpl.java
+++ microservice/user-service/src/main/java/com/anudari/user_service/serviceImpl/AccountServiceImpl.java
@@ -39,83 +39,99 @@ public class AccountServiceImpl implements AccountService {
     @Override
     @Transactional
     public AccountResponse createAccount(Long userId, CreateAccountRequest request) {
-        LogUtility.info(this.getClass().getName(), String.valueOf(userId), "ACCOUNT",
-                "[create.account] " + JSONUtility.toJSON(request));
+        LogUtility.info(this.getClass().getName(), String.valueOf(userId), "ACCOUNT", "[create.account] " + JSONUtility.toJSON(request));
         try {
-            User user = userRepository.findById(userId)
-                    .orElseThrow(() -> new NoSuchElementException(MessageUtility.getMessage("user.not.found")));
-
+            User user = userRepository.findById(userId).orElseThrow(() -> new NoSuchElementException(MessageUtility.getMessage("user.not.found")));
             if (accountRepository.existsByUser_UserIdAndCurrency(userId, request.currency())) {
-                throw new IllegalStateException(
-                        MessageUtility.getMessage("account.exists", new Object[]{request.currency()})
-                );
+                throw new IllegalStateException(MessageUtility.getMessage("account.exists", new Object[]{request.currency()}));
             }
-
             Account account = Account.builder()
                     .accountNumber(generateUniqueAccountNumber())
                     .currency(request.currency())
                     .user(user)
                     .build();
-
             AccountResponse response = AccountResponse.from(accountRepository.save(account));
-
-            LogUtility.info(this.getClass().getName(), String.valueOf(userId), "ACCOUNT",
-                    "[create.account] " + JSONUtility.toJSON(response));
-
+            LogUtility.info(this.getClass().getName(), String.valueOf(userId), "ACCOUNT", "[create.account] " + JSONUtility.toJSON(response));
             return response;
         } catch (Exception ex) {
-            LogUtility.error(this.getClass().getName(), String.valueOf(userId), "ACCOUNT",
-                    "[create.account] Exception: " + ex.getMessage());
+            LogUtility.error(this.getClass().getName(), String.valueOf(userId), "ACCOUNT", "[create.account] Exception: " + ex.getMessage());
             throw ex;
         }
     }
 
     @Override
     public List<AccountResponse> getAccountsByUser(Long userId, Long requesterId, String isAdmin) {
-        checkAccess(userId, requesterId, isAdmin);
-        if (!userRepository.existsById(userId)) {
-            throw new NoSuchElementException(MessageUtility.getMessage("user.not.found"));
+        LogUtility.info(this.getClass().getName(), String.valueOf(userId), "ACCOUNT", "[get.accounts.by.user] requesterId: " + requesterId + ", isAdmin: " + isAdmin);
+        try {
+            checkAccess(userId, requesterId, isAdmin);
+            if (!userRepository.existsById(userId)) {
+                throw new NoSuchElementException(MessageUtility.getMessage("user.not.found"));
+            }
+            List<AccountResponse> responses = accountRepository.findAllByUser_UserId(userId).stream()
+                    .map(AccountResponse::from)
+                    .toList();
+            LogUtility.info(this.getClass().getName(), String.valueOf(userId), "ACCOUNT", "[get.accounts.by.user] " + JSONUtility.toJSON(responses));
+            return responses;
+        } catch (Exception ex) {
+            LogUtility.error(this.getClass().getName(), String.valueOf(userId), "ACCOUNT", "[get.accounts.by.user] Exception: " + ex.getMessage());
+            throw ex;
         }
-        return accountRepository.findAllByUser_UserId(userId).stream()
-                .map(AccountResponse::from)
-                .toList();
     }
 
     @Override
     public AccountResponse getAccount(Long accountId, Long requesterId, String isAdmin) {
-        Account account = findAccount(accountId);
-        checkAccess(account.getUser().getUserId(), requesterId, isAdmin);
-        return AccountResponse.from(account);
+        LogUtility.info(this.getClass().getName(), String.valueOf(requesterId), "ACCOUNT", "[get.account] accountId: " + accountId + ", isAdmin: " + isAdmin);
+        try {
+            Account account = findAccount(accountId);
+            checkAccess(account.getUser().getUserId(), requesterId, isAdmin);
+            AccountResponse response = AccountResponse.from(account);
+            LogUtility.info(this.getClass().getName(), String.valueOf(requesterId), "ACCOUNT", "[get.account] " + JSONUtility.toJSON(response));
+            return response;
+        } catch (Exception ex) {
+            LogUtility.error(this.getClass().getName(), String.valueOf(requesterId), "ACCOUNT", "[get.account] Exception: " + ex.getMessage());
+            throw ex;
+        }
     }
 
     @Override
     @Transactional
     public AccountResponse deposit(Long accountId, AccountTransactionRequest request, Long requesterId, String isAdmin) {
-        Account account = findAccount(accountId);
-        checkAccess(account.getUser().getUserId(), requesterId, isAdmin);
-
-        BigDecimal before = account.getBalance();
-        account.setBalance(before.add(request.amount()));
-        accountRepository.save(account);
-
-        recordTransaction(account, TransactionType.DEPOSIT, request.amount(), before, account.getBalance(), "Deposit");
-        return AccountResponse.from(account);
+        LogUtility.info(this.getClass().getName(), String.valueOf(requesterId), "ACCOUNT", "[deposit] accountId: " + accountId + ", request: " + JSONUtility.toJSON(request) + ", isAdmin: " + isAdmin);
+        try {
+            Account account = findAccount(accountId);
+            checkAccess(account.getUser().getUserId(), requesterId, isAdmin);
+            BigDecimal before = account.getBalance();
+            account.setBalance(before.add(request.amount()));
+            accountRepository.save(account);
+            recordTransaction(account, TransactionType.DEPOSIT, request.amount(), before, account.getBalance(), "Deposit");
+            AccountResponse response = AccountResponse.from(account);
+            LogUtility.info(this.getClass().getName(), String.valueOf(requesterId), "ACCOUNT", "[deposit] " + JSONUtility.toJSON(response));
+            return response;
+        } catch (Exception ex) {
+            LogUtility.error(this.getClass().getName(), String.valueOf(requesterId), "ACCOUNT", "[deposit] Exception: " + ex.getMessage());
+            throw ex;
+        }
     }
 
     @Override
     @Transactional
     public AccountResponse withdraw(Long accountId, AccountTransactionRequest request, Long requesterId, String isAdmin) {
-        Account account = findAccount(accountId);
-        checkAccess(account.getUser().getUserId(), requesterId, isAdmin);
-
-        checkSufficientBalance(account, request.amount());
-
-        BigDecimal before = account.getBalance();
-        account.setBalance(before.subtract(request.amount()));
-        accountRepository.save(account);
-
-        recordTransaction(account, TransactionType.WITHDRAW, request.amount(), before, account.getBalance(), "Withdrawal");
-        return AccountResponse.from(account);
+        LogUtility.info(this.getClass().getName(), String.valueOf(requesterId), "ACCOUNT", "[withdraw] accountId: " + accountId + ", request: " + JSONUtility.toJSON(request) + ", isAdmin: " + isAdmin);
+        try {
+            Account account = findAccount(accountId);
+            checkAccess(account.getUser().getUserId(), requesterId, isAdmin);
+            checkSufficientBalance(account, request.amount());
+            BigDecimal before = account.getBalance();
+            account.setBalance(before.subtract(request.amount()));
+            accountRepository.save(account);
+            recordTransaction(account, TransactionType.WITHDRAW, request.amount(), before, account.getBalance(), "Withdrawal");
+            AccountResponse response = AccountResponse.from(account);
+            LogUtility.info(this.getClass().getName(), String.valueOf(requesterId), "ACCOUNT", "[withdraw] " + JSONUtility.toJSON(response));
+            return response;
+        } catch (Exception ex) {
+            LogUtility.error(this.getClass().getName(), String.valueOf(requesterId), "ACCOUNT", "[withdraw] Exception: " + ex.getMessage());
+            throw ex;
+        }
     }
 
     private void checkSufficientBalance(Account account, BigDecimal requestedAmount) {
@@ -141,56 +157,71 @@ public class AccountServiceImpl implements AccountService {
     @Override
     @Transactional
     public void debitForInvoice(DebitRequest request, String secretToken) {
-        Account account = validateAndGetAccount(request.accountId(), request.userId(), secretToken);
-
-        checkSufficientBalance(account, request.amount());
-
-        BigDecimal before = account.getBalance();
-        account.setBalance(before.subtract(request.amount()));
-        accountRepository.save(account);
-
-        recordTransaction(account, TransactionType.INVOICE_DEBIT, request.amount(), before, account.getBalance(), "Invoice payment");
+        LogUtility.info(this.getClass().getName(), String.valueOf(request.userId()), "ACCOUNT", "[debit.for.invoice] " + JSONUtility.toJSON(request));
+        try {
+            Account account = validateAndGetAccount(request.accountId(), request.userId(), secretToken);
+            checkSufficientBalance(account, request.amount());
+            BigDecimal before = account.getBalance();
+            account.setBalance(before.subtract(request.amount()));
+            accountRepository.save(account);
+            recordTransaction(account, TransactionType.INVOICE_DEBIT, request.amount(), before, account.getBalance(), "Invoice payment");
+            LogUtility.info(this.getClass().getName(), String.valueOf(request.userId()), "ACCOUNT", "[debit.for.invoice] " + JSONUtility.toJSON(AccountResponse.from(account)));
+        } catch (Exception ex) {
+            LogUtility.error(this.getClass().getName(), String.valueOf(request.userId()), "ACCOUNT", "[debit.for.invoice] Exception: " + ex.getMessage());
+            throw ex;
+        }
     }
 
     @Override
     @Transactional
     public void creditForInvoice(CreditRequest request, String secretToken) {
-        Account account = validateAndGetAccount(request.accountId(), request.userId(), secretToken);
-
-        BigDecimal before = account.getBalance();
-        account.setBalance(before.add(request.amount()));
-        accountRepository.save(account);
-
-        recordTransaction(account, TransactionType.INVOICE_CREDIT, request.amount(), before, account.getBalance(), "Invoice payment");
+        LogUtility.info(this.getClass().getName(), String.valueOf(request.userId()), "ACCOUNT", "[credit.for.invoice] " + JSONUtility.toJSON(request));
+        try {
+            Account account = validateAndGetAccount(request.accountId(), request.userId(), secretToken);
+            BigDecimal before = account.getBalance();
+            account.setBalance(before.add(request.amount()));
+            accountRepository.save(account);
+            recordTransaction(account, TransactionType.INVOICE_CREDIT, request.amount(), before, account.getBalance(), "Invoice payment");
+            LogUtility.info(this.getClass().getName(), String.valueOf(request.userId()), "ACCOUNT", "[credit.for.invoice] " + JSONUtility.toJSON(AccountResponse.from(account)));
+        } catch (Exception ex) {
+            LogUtility.error(this.getClass().getName(), String.valueOf(request.userId()), "ACCOUNT", "[credit.for.invoice] Exception: " + ex.getMessage());
+            throw ex;
+        }
     }
 
     private Account validateAndGetAccount(Long accountId, Long userId, String secretToken) {
         if (secretToken == null || !secretToken.equals(appProperties.getInternalSecret())) {
             throw new SecurityException(MessageUtility.getMessage("secret.invalid"));
         }
-
         Account account = accountRepository.findById(accountId)
                 .orElseThrow(() -> new NoSuchElementException(MessageUtility.getMessage("account.not.found", new Object[]{accountId})));
-
         if (!account.getUser().getUserId().equals(userId)) {
             throw new SecurityException(MessageUtility.getMessage("account.ownership.mismatch"));
         }
-
         return account;
     }
 
     @Override
     public List<AccountTransactionResponse> getTransactions(Long accountId, Long requesterId, String isAdmin) {
-        Account account = findAccount(accountId);
-        checkAccess(account.getUser().getUserId(), requesterId, isAdmin);
-        return transactionRepository.findAllByAccount_AccountIdOrderByCreatedAtDesc(accountId)
-                .stream()
-                .map(AccountTransactionResponse::from)
-                .toList();
+        LogUtility.info(this.getClass().getName(), String.valueOf(requesterId), "ACCOUNT", "[get.transactions] accountId: " + accountId + ", isAdmin: " + isAdmin);
+        try {
+            Account account = findAccount(accountId);
+            checkAccess(account.getUser().getUserId(), requesterId, isAdmin);
+
+            List<AccountTransactionResponse> responses = transactionRepository.findAllByAccount_AccountIdOrderByCreatedAtDesc(accountId)
+                    .stream()
+                    .map(AccountTransactionResponse::from)
+                    .toList();
+
+            LogUtility.info(this.getClass().getName(), String.valueOf(requesterId), "ACCOUNT", "[get.transactions] " + JSONUtility.toJSON(responses));
+            return responses;
+        } catch (Exception ex) {
+            LogUtility.error(this.getClass().getName(), String.valueOf(requesterId), "ACCOUNT", "[get.transactions] Exception: " + ex.getMessage());
+            throw ex;
+        }
     }
 
-    private void recordTransaction(Account account, TransactionType type, BigDecimal amount,
-                                   BigDecimal before, BigDecimal after, String description) {
+    private void recordTransaction(Account account, TransactionType type, BigDecimal amount, BigDecimal before, BigDecimal after, String description) {
         transactionRepository.save(AccountTransaction.builder()
                 .account(account)
                 .type(type)
--- microservice/user-service/src/main/java/com/anudari/user_service/serviceImpl/UserServiceImpl.java
+++ microservice/user-service/src/main/java/com/anudari/user_service/serviceImpl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.anudari.user_service.serviceImpl;
 
 import com.anudari.common.constant.AppConstants;
+import com.anudari.common.utility.LogUtility;
+import com.anudari.common.utility.JSONUtility;
 import com.anudari.user_service.config.AppProperties;
 import com.anudari.user_service.dto.RegisterRequest;
 import com.anudari.user_service.dto.UpdateUserRequest;
@@ -15,7 +17,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -32,73 +33,146 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserResponse register(RegisterRequest request) {
-        User user = new User();
-        user.setUsername(request.username());
-        user.setEmail(request.email());
-        user.setPhoneNumber(request.phoneNumber());
-        user.setPassword(passwordEncoder.encode(request.password()));
-        user.setRoles(Set.of(AppConstants.ROLE.USER));
-        return UserResponse.from(userRepository.save(user));
+        LogUtility.info(this.getClass().getName(), request.username(), "USER", "[register] " + JSONUtility.toJSON(request));
+        try {
+            User user = new User();
+            user.setUsername(request.username());
+            user.setEmail(request.email());
+            user.setPhoneNumber(request.phoneNumber());
+            user.setPassword(passwordEncoder.encode(request.password()));
+            user.setRoles(Set.of(AppConstants.ROLE.USER));
+
+            UserResponse response = UserResponse.from(userRepository.save(user));
+
+            LogUtility.info(this.getClass().getName(), request.username(), "USER", "[register] " + JSONUtility.toJSON(response));
+            return response;
+        } catch (Exception ex) {
+            LogUtility.error(this.getClass().getName(), request.username(), "USER", "[register] Exception: " + ex.getMessage());
+            throw ex;
+        }
     }
 
     @Override
     public UserResponse getUserById(Long id) {
-        return userRepository.findById(id)
-                .map(UserResponse::from)
-                .orElseThrow(() -> new NoSuchElementException(MessageUtility.getMessage("user.not.found")));
+        LogUtility.info(this.getClass().getName(), String.valueOf(id), "USER", "[get.user.by.id] id: " + id);
+        try {
+            UserResponse response = userRepository.findById(id)
+                    .map(UserResponse::from)
+                    .orElseThrow(() -> new NoSuchElementException(MessageUtility.getMessage("user.not.found")));
+
+            LogUtility.info(this.getClass().getName(), String.valueOf(id), "USER", "[get.user.by.id] " + JSONUtility.toJSON(response));
+            return response;
+        } catch (Exception ex) {
+            LogUtility.error(this.getClass().getName(), String.valueOf(id), "USER", "[get.user.by.id] Exception: " + ex.getMessage());
+            throw ex;
+        }
     }
 
     @Override
     public UserResponse updateUser(Long id, UpdateUserRequest request) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new NoSuchElementException(MessageUtility.getMessage("user.not.found")));
-        user.setEmail(request.email());
-        return UserResponse.from(userRepository.save(user));
+        LogUtility.info(this.getClass().getName(), String.valueOf(id), "USER", "[update.user] " + JSONUtility.toJSON(request));
+        try {
+            User user = userRepository.findById(id)
+                    .orElseThrow(() -> new NoSuchElementException(MessageUtility.getMessage("user.not.found")));
+            user.setEmail(request.email());
+
+            UserResponse response = UserResponse.from(userRepository.save(user));
+
+            LogUtility.info(this.getClass().getName(), String.valueOf(id), "USER", "[update.user] " + JSONUtility.toJSON(response));
+            return response;
+        } catch (Exception ex) {
+            LogUtility.error(this.getClass().getName(), String.valueOf(id), "USER", "[update.user] Exception: " + ex.getMessage());
+            throw ex;
+        }
     }
 
     @Override
     public UserResponse getUser(String username) {
-        return userRepository.findByUsername(username)
-                .map(UserResponse::from)
-                .orElseThrow(() -> new NoSuchElementException(MessageUtility.getMessage("user.not.found")));
+        LogUtility.info(this.getClass().getName(), username, "USER", "[get.user] username: " + username);
+        try {
+            UserResponse response = userRepository.findByUsername(username)
+                    .map(UserResponse::from)
+                    .orElseThrow(() -> new NoSuchElementException(MessageUtility.getMessage("user.not.found")));
+
+            LogUtility.info(this.getClass().getName(), username, "USER", "[get.user] " + JSONUtility.toJSON(response));
+            return response;
+        } catch (Exception ex) {
+            LogUtility.error(this.getClass().getName(), username, "USER", "[get.user] Exception: " + ex.getMessage());
+            throw ex;
+        }
     }
 
     @Override
     public UserLookupResponse lookupByPhone(String phoneNumber) {
-        return userRepository.findByPhoneNumber(phoneNumber)
-                .map(UserLookupResponse::from)
-                .orElseThrow(() -> new NoSuchElementException(MessageUtility.getMessage("user.not.found")));
+        LogUtility.info(this.getClass().getName(), phoneNumber, "USER", "[lookup.by.phone] phoneNumber: " + phoneNumber);
+        try {
+            UserLookupResponse response = userRepository.findByPhoneNumber(phoneNumber)
+                    .map(UserLookupResponse::from)
+                    .orElseThrow(() -> new NoSuchElementException(MessageUtility.getMessage("user.not.found")));
+
+            LogUtility.info(this.getClass().getName(), phoneNumber, "USER", "[lookup.by.phone] " + JSONUtility.toJSON(response));
+            return response;
+        } catch (Exception ex) {
+            LogUtility.error(this.getClass().getName(), phoneNumber, "USER", "[lookup.by.phone] Exception: " + ex.getMessage());
+            throw ex;
+        }
     }
 
     @Override
     public UserInternalResponse internalSearch(String username, String secretToken) {
-        if (secretToken == null || !secretToken.equals(appProperties.getInternalSecret())) {
-            throw new SecurityException(MessageUtility.getMessage("access.denied"));
+        LogUtility.info(this.getClass().getName(), username, "USER", "[internal.search] username: " + username);
+        try {
+            if (secretToken == null || !secretToken.equals(appProperties.getInternalSecret())) {
+                throw new SecurityException(MessageUtility.getMessage("access.denied"));
+            }
+            UserInternalResponse response = userRepository.findByUsername(username)
+                    .map(UserInternalResponse::from)
+                    .orElseThrow(() -> new NoSuchElementException(MessageUtility.getMessage("user.not.found")));
+
+            LogUtility.info(this.getClass().getName(), username, "USER", "[internal.search] " + JSONUtility.toJSON(response));
+            return response;
+        } catch (Exception ex) {
+            LogUtility.error(this.getClass().getName(), username, "USER", "[internal.search] Exception: " + ex.getMessage());
+            throw ex;
         }
-        return userRepository.findByUsername(username)
-                .map(UserInternalResponse::from)
-                .orElseThrow(() -> new NoSuchElementException(MessageUtility.getMessage("user.not.found")));
     }
 
     @Override
     public UserInternalResponse internalSearchByPhone(String phoneNumber, String secretToken) {
-        if (secretToken == null || !secretToken.equals(appProperties.getInternalSecret())) {
-            throw new SecurityException(MessageUtility.getMessage("access.denied"));
+        LogUtility.info(this.getClass().getName(), phoneNumber, "USER", "[internal.search.by.phone] phoneNumber: " + phoneNumber);
+        try {
+            if (secretToken == null || !secretToken.equals(appProperties.getInternalSecret())) {
+                throw new SecurityException(MessageUtility.getMessage("access.denied"));
+            }
+            UserInternalResponse response = userRepository.findByPhoneNumber(phoneNumber)
+                    .map(UserInternalResponse::from)
+                    .orElseThrow(() -> new NoSuchElementException(MessageUtility.getMessage("user.not.found.phone", new Object[]{phoneNumber})));
+
+            LogUtility.info(this.getClass().getName(), phoneNumber, "USER", "[internal.search.by.phone] " + JSONUtility.toJSON(response));
+            return response;
+        } catch (Exception ex) {
+            LogUtility.error(this.getClass().getName(), phoneNumber, "USER", "[internal.search.by.phone] Exception: " + ex.getMessage());
+            throw ex;
         }
-        return userRepository.findByPhoneNumber(phoneNumber)
-                .map(UserInternalResponse::from)
-                .orElseThrow(() -> new NoSuchElementException(MessageUtility.getMessage("user.not.found.phone", new Object[]{phoneNumber})));
     }
 
     @Async("asyncExecutor")
     @Override
     public CompletableFuture<List<UserResponse>> listAllUsers(String secretToken) {
-        if (secretToken == null || !secretToken.equals(appProperties.getInternalSecret())) {
-            throw new SecurityException(MessageUtility.getMessage("access.denied"));
+        LogUtility.info(this.getClass().getName(), "SYSTEM", "USER", "[list.all.users] request received");
+        try {
+            if (secretToken == null || !secretToken.equals(appProperties.getInternalSecret())) {
+                throw new SecurityException(MessageUtility.getMessage("access.denied"));
+            }
+            List<UserResponse> users = userRepository.findAll().stream()
+                    .map(UserResponse::from)
+                    .toList();
+
+            LogUtility.info(this.getClass().getName(), "SYSTEM", "USER", "[list.all.users] count: " + users.size());
+            return CompletableFuture.completedFuture(users);
+        } catch (Exception ex) {
+            LogUtility.error(this.getClass().getName(), "SYSTEM", "USER", "[list.all.users] Exception: " + ex.getMessage());
+            throw ex;
         }
-        List<UserResponse> users = userRepository.findAll().stream()
-                .map(UserResponse::from)
-                .toList();
-        return CompletableFuture.completedFuture(users);
     }
 }
